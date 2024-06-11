f592335436bf33c0202271052c33d806de0c82e3
Bryan el chico de mkt <emmanuelbryan1999@gmail.com>
2024-06-11 10:05:47 -0600

    comentarios prueba unitarias

--- categorias/index.php
+++ categorias/index.php
@@ -5,7 +5,7 @@ include('../app/controllers/categorias/listado_de_categorias.php');
 include('../layaout/parte1.php'); 
 ?>
 
-<?php /*holaaaaaaaagit push
+<?php /* sesion reactivar cuando se requiera
 if(isset($_SESSION['mensaje'])){
   $respuesta = $_SESSION['mensaje']; ?>
 
